dc4ea1083f0496c2466c6a7cf1b3cc2566fe7a96
tnauss <nauss@staff.uni-marburg.de>
2018-10-06 20:30:52 +0200

    Include MNF and PCA in noise removal

--- src/030_noise_removal.R
+++ src/030_noise_removal.R
@@ -1,4 +1,4 @@
-# Compute noise removal on a per plot basis.
+# Compute noise removal on a per plot basis
 
 source("C:/Users/tnauss/permanent/plygrnd/KI-Hyperspec/HySpec_KiLi/src/000_set_environment.R")
 
@@ -9,57 +9,53 @@ pb = shapefile(paste0(path_plots, "BPolygon.shp"))
 
 dir.create(paste0(path_hyp_nrm), showWarnings = FALSE)
 
+# for(f in hd_files){
+#  r = readRDS(f)
+#  
+#  m = mnf(as(r, "SpatialGridDataFrame"), use = "complete.obs")
+#  
+#  thv = 1-m$values
+#  set_mean = which(thv < -0.10)
+#  
+#  for(i in use){
+#    m$x@data[,i] = mean(m$x@data[,i], na.rm = TRUE)
+#  }
+#  mi = as.matrix(m$x@data) %*% solve(m$rotation)
+#  tmp = r[[1]]
+#  mir = stack(lapply(seq(ncol(mi)), function(i){
+#    setValues(tmp, mi[, i])
+#  }))
+# 
+#  saveRDS(mir, file = paste0(path_hyp_nrm, substr(basename(f), 1, 4), "_mnfi.rds"))
+# }
+
+log = list()
+log_entry = 0
+
 for(f in hd_files){
- r = readRDS(f)
- 
- m = mnf(as(r, "SpatialGridDataFrame"), use = "complete.obs")
- 
- thv = 1-m$values
- use = which(thv > 0.1)
- spplot(m$x[160])
- 
- for(i in use){
-   m$x@data[,i] = 0 #mean(m$x@data[,i], na.rm = TRUE)
- }
- # focal(r, w=matrix(1/9,nrow=3,ncol=3)) 
- 
- # plot(m$values)
- # mi = as.matrix(m$x@data) %*% solve(m$rotation)
- mi = as.matrix(m$x@data)[, use] %*% solve(m$rotation)[use, ]
- # roti = solve(e$rotation)
- # xi = e$x[, -1] %*% roti[-1, ]
- # xi = e$x %*% roti
- tmp = r[[1]]
- mir = stack(lapply(seq(ncol(mi)), function(i){
-   setValues(tmp, mi[, i])
- }))
- plot(mir)
- plot(r)
- 
- summary(mir[[108]] / r[[108]])
- 
- mir[[108]][4002]
- r[[108]][4002]
- 
- xr = as(m$x, "RasterStack")
- writeRaster(xr, paste0(path_temp, "m.tif"), "GTiff", overwrite = TRUE)
- writeRaster(r, paste0(path_temp, "r.tif"), "GTiff", overwrite = TRUE)
+  r = readRDS(f)
+  
+  all_na = grep(ncell(r), summary(r)[6,])
+  if(length(all_na) > 0){
+    log_entry = log_entry + 1
+    log[[log_entry]] = list(file = basename(f), all_na = all_na)
+    r = r[[-all_na]]
+  }
+  
+  pca = rasterPCA(r)
+  v = pca$model$sdev**2
+  
+  # Continuous Significant Dimensionality 
+  csd = round(sum(sapply(v, function(x){min(x,1)})), 0)
+  use = seq(csd)
+  pcai = t(t(as.matrix(pca$map)[, use] %*% t(pca$model$loadings)[use, ]) + pca$model$center)
+  
+  tmp = r[[1]]
+  pcair = stack(lapply(seq(ncol(pcai)), function(i){
+    setValues(tmp, pcai[, i])
+  }))
+
+  saveRDS(pcair, file = paste0(path_hyp_nrm, substr(basename(f), 1, 4), "_pcai.rds"))
 }
 
-
-
-
-
-
-pca = rasterPCA(r)
-v = pca$model$sdev**2
-thv = v-1
-use = which(thv > 2)
-pcai = t(t(as.matrix(pca$map)[, use] %*% t(pca$model$loadings)[use, ]) + pca$model$center)
-
-tmp = r[[1]]
-pcair = stack(lapply(seq(ncol(pcai)), function(i){
-  setValues(tmp, pcai[, i])
-}))
-plot(pcair[[108]])
-plot(r[[108]])
+saveRDS(log, file = paste0(path_meta, "030_noise_removal_log.rds"))
