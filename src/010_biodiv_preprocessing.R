dfc6044594780d03f931883d37f0f1962039af25
tnauss <nauss@staff.uni-marburg.de>
2018-11-18 19:08:06 +0100

    Update 010_biodiv_preprocessing.R

--- src/010_biodiv_preprocessing.R
+++ src/010_biodiv_preprocessing.R
@@ -2,13 +2,11 @@
 
 source("C:/Users/tnauss/permanent/plygrnd/KI-Hyperspec/HySpec_KiLi/src/000_set_environment.R")
 
-# Read dataset
+# Read species richness dataset
 bd = read.table(paste0(path_biodiv, "Biodiversity_Data_Marcel.csv"),
                 header = TRUE, sep = ";", dec = ",")
 
 saveRDS(as.character(bd$plotID), file = paste0(path_biodiv, "biodiv_plots.rds"))
 saveRDS(bd, file = paste0(path_biodiv, "biodiv.rds"))
-
-
 
 
